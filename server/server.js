b41e69566ff17ff763f9357e3c9aac658048c2ce
Summer Huang <13524382551@163.com>
2026-03-15 18:47:40 +0800

    fix:server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const http = require("http");
+const path = require('path');
 const createWebSocketServer = require('./ws');
 const connectMongo = require('./mongo');
 
