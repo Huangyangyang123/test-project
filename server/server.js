ee8986079df5273c65c5b6590642c7eb133f74e4
Summer Huang <13524382551@163.com>
2026-03-22 21:18:59 +0800

    test

--- server/server.js
+++ server/server.js
@@ -1,29 +1,10 @@
-// Load environment variables from .env (optional)
-// require('dotenv').config();
-
 const express = require('express');
-// const cors = require('cors');
 const path = require('path');
-// const createWebSocketServer = require('./ws');
-// const connectMongo = require('./mongo');
 
 const app = express();
 
 // 提供前端生产文件
 app.use(express.static(path.join(__dirname, '../dist')));
-
-// app.use(cors());
-// app.use(express.json());
-// // connectMongo();
-// const server = http.createServer(app);
-// createWebSocketServer(server);
-
-// 获取聊天记录 API
-// app.get("/history/:room", async (req, res) => {
-//   const Message = require('./models/Message');
-//   const logs = await Message.find({ room: req.params.room });
-//   res.json(logs);
-// });
 
 // 启动
 app.listen(3001, '0.0.0.0', () => {
