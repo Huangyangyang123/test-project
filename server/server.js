beaa77cbe76b8a30df891af1fe7d56da0a5db504
Summer Huang <13524382551@163.com>
2026-03-22 21:03:59 +0800

    fix:fixed code

--- server/server.js
+++ server/server.js
@@ -26,6 +26,6 @@ app.use(express.static(path.join(__dirname, '../dist')));
 // });
 
 // 启动
-app.listen(3001, () => {
+app.listen(3001, '0.0.0.0', () => {
   console.log("服务器已启动：http://localhost:3001");
 });
